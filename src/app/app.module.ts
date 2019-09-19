844c65073c4bfec5d5b803fa14d45d1f06a5ddff
Cristian221995 <crisfonteina@gmail.com>
2019-09-19 13:53:13 -0300

    Log in con local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { NoticeComponent} from './components/notice/notice.component';
 import { FormNoticeComponent } from './components/form-notice/form-notice.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { appRoutingModule } from './app.routing';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +23,22 @@ import { appRoutingModule } from './app.routing';
     NoticeComponent,
     FormNoticeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavComponent,
+    HomeComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     appRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
