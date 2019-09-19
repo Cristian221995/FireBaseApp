50d748025483b0e7efe50b20fefb6817eed921d4
maxu99 <maxuu1999@gmail.com>
2019-09-19 11:41:06 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoticeComponent } from './components/notice/notice.component';
+import { FormNoticeComponent } from './components/form-notice/form-notice.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticeComponent,
+    FormNoticeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
